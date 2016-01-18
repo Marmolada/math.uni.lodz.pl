4160d17b8bf42557d0626460a8cfaca91b4d4c55
Andrzej Kowalczyk <greynevan1994@gmail.com>
2016-01-18 19:07:14 +0100

    homework from lesson 2 done
    completely missed that one due to being sick that time and forgot later
    to catch up

--- lesson-2/src/test/java/com/gft/structuraltest/testconference/registrationdata/AnswerEntityTest.java
+++ lesson-2/src/test/java/com/gft/structuraltest/testconference/registrationdata/AnswerEntityTest.java
@@ -12,4 +12,85 @@ public class AnswerEntityTest{
         assertFalse(an1.equals(an2));
     }
     
+	@Test
+    public void shouldReturnTrueWhenObjectsAreSame(){
+        AnswerEntity an1 = new AnswerEntity();
+        AnswerEntity an2 = an1;
+		assertTrue(an1.equals(an2));
+    }
+	
+	@Test
+    public void shouldReturnTrueWhenObjectsAreSameClass(){
+        AnswerEntity an1 = new AnswerEntity();
+        String an2 = new String();
+		assertTrue(an1.equals(an2));
+    }
+	
+	@Test
+    public void shouldReturnTrueWhenAreEqual(){
+		QuestionEntity qe = new QuestionEntity();
+		
+        AnswerEntity an1 = new AnswerEntity();
+		an1.setId(10);
+		an1.setAnswerText("answer");
+		an1.setQuestion(qe);
+		
+        AnswerEntity an2 = new AnswerEntity();
+		an2.setId(10);
+		an2.setAnswerText("answer");
+		an2.setQuestion(qe);
+		
+		assertTrue(an1.equals(an2));
+    }
+	
+	@Test
+    public void shouldReturnFalseWhenIDsArentEqual(){
+		QuestionEntity qe = new QuestionEntity();
+		
+        AnswerEntity an1 = new AnswerEntity();
+		an1.setAnswerText("answer");
+		an1.setQuestion(qe);
+		
+        AnswerEntity an2 = new AnswerEntity();
+		an2.setId(10);
+		an2.setAnswerText("answer");
+		an2.setQuestion(qe);
+		
+		assertFalse(an1.equals(an2));
+    }
+	
+	@Test
+    public void shouldReturnFalseWhenAnsTextsArentEqual(){
+		QuestionEntity qe = new QuestionEntity();
+		
+        AnswerEntity an1 = new AnswerEntity();
+		an1.setId(10);
+		an1.setQuestion(qe);
+		
+        AnswerEntity an2 = new AnswerEntity();
+		an2.setId(10);
+		an2.setAnswerText("answer");
+		an2.setQuestion(qe);
+		
+		assertFalse(an1.equals(an2));
+    }
+	
+	@Test
+    public void shouldReturnFalseWhenQuestionsArentEqual(){
+		QuestionEntity qe = new QuestionEntity();
+		
+		QuestionEntity qe2 = new QuestionEntity();
+		qe2.setQuestionText("questionText");
+		
+        AnswerEntity an1 = new AnswerEntity();
+		an1.setId(10);
+		an1.setAnswerText("answer");
+		an2.setQuestion(qe);
+		
+        AnswerEntity an2 = new AnswerEntity();
+		an2.setId(10);
+		an2.setAnswerText("answer");
+		an2.setQuestion(qe2);
+		assertFalse(an1.equals(an2));
+    }
 }
